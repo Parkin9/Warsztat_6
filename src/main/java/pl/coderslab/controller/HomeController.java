bba5498f8f53b428d23c3c8fbea69da4c2b84c71
Piotr <piotrparkola@gmail.com>
2018-06-08 17:57:53 +0200

    Some cleaning

--- src/main/java/pl/coderslab/controller/HomeController.java
+++ src/main/java/pl/coderslab/controller/HomeController.java
@@ -3,8 +3,6 @@ package pl.coderslab.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 public class HomeController {
 
--- src/main/java/pl/coderslab/controller/UserController.java
+++ src/main/java/pl/coderslab/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.model.Login;
-import pl.coderslab.model.Tweet;
 import pl.coderslab.model.User;
 import pl.coderslab.repository.TweetRepository;
 import pl.coderslab.repository.UserRepository;
